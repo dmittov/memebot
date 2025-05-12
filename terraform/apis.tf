d35857d252922565ef6cbe17fe651ad7ad90d1c2
Dmitry Mittov <mittov@gmail.com>
2025-05-12 18:33:55 +0200

    resources

---
+++ terraform/apis.tf
@@ -0,0 +1,12 @@
+resource "google_project_service" "required_apis" {
+  for_each = toset([
+    "appengine.googleapis.com",
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "serviceusage.googleapis.com",
+  ])
+  project                    = data.google_client_config.default.project
+  service                    = each.key
+  disable_on_destroy         = false
+  disable_dependent_services = false
+}
