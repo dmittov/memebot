43cb73ef7d20340621e5f90e927c6c3e29fece00
Dmitry Mittov <mittov@gmail.com>
2025-06-04 14:09:44 +0200

    enable generativelanguage

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -7,6 +7,7 @@ resource "google_project_service" "required_apis" {
     "secretmanager.googleapis.com",
     "firestore.googleapis.com",
     "aiplatform.googleapis.com",
+    "generativelanguage.googleapis.com",
   ])
   project                    = data.google_client_config.default.project
   service                    = each.key
