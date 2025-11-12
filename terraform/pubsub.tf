938763b8e7841c452c2df8ffc11fe6e4f52833c7
Dmitry Mittov <mittov@gmail.com>
2025-11-12 22:26:20 +0100

    enable_exactly_once_delivery

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -7,6 +7,8 @@ resource "google_pubsub_subscription" "sub_explain" {
   name  = "sub-explain-pull"
   topic = google_pubsub_topic.topic_explain.name
 
-  ack_deadline_seconds       = 600
-  retain_acked_messages      = false
+  ack_deadline_seconds         = 600
+  retain_acked_messages        = false
+  enable_exactly_once_delivery = true
+
 }
