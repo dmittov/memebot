57d98d6c2d6010b8889959cb97808177b99b9dea
Dmitry Mittov <mittov@gmail.com>
2025-11-13 14:19:59 +0100

    Fastapi (#18)
    Flask --> FastAPI

--- memebot/tests/conftest.py
+++ memebot/tests/conftest.py
@@ -2,17 +2,16 @@ import datetime
 from typing import Generator
 
 import pytest
-from flask.testing import FlaskClient
+from fastapi.testclient import TestClient
 from telegram import Message
 
-from main import app as flask_app
+from main import app
 
 
 @pytest.fixture
-def client() -> Generator[FlaskClient, None, None]:
-    flask_app.config.update(TESTING=True)
-    with flask_app.test_client() as client:
-        yield client
+def client() -> Generator[TestClient, None, None]:
+    client = TestClient(app)
+    yield client
 
 
 @pytest.fixture
