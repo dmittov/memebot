beec6711fc925adf75056d242ad2b443d7b70192
Dmitry Mittov <mittov@gmail.com>
2025-11-14 17:55:12 +0100

    pub/sub emulator fixture

--- memebot/tests/conftest.py
+++ memebot/tests/conftest.py
@@ -1,8 +1,15 @@
+import asyncio
+from collections.abc import AsyncGenerator
+import contextlib
 import datetime
+import os
+import socket
+import time
 from typing import Generator
 
 import pytest
 from fastapi.testclient import TestClient
+from pytest_mock import MockerFixture
 from telegram import Message
 
 from main import app
@@ -26,3 +33,58 @@ def message() -> Generator[Message, None, None]:
         }
     )
     yield message
+
+
+@pytest.fixture
+async def pubsub(mocker: MockerFixture) -> AsyncGenerator[asyncio.subprocess.Process]:
+    # install emulator:
+    # $ gcloud components install beta
+    # $ gcloud components install pubsub-emulator
+    # Please note pubsub-emulator requires Java
+    host = "localhost"
+    port = 8085  # default emulator's port
+    timeout_sec = 10.0
+    proc = await asyncio.create_subprocess_exec(
+        "gcloud",
+        "beta",
+        "emulators",
+        "pubsub",
+        "start",
+        f"--host-port={host}:{port}",
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+
+    async def wait_for_emulator() -> None:
+        # wait for emulator is ready
+        start_time = asyncio.get_event_loop().time()
+        while (asyncio.get_event_loop().time() - start_time) > timeout_sec:
+            try:
+                with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+                    sock.settimeout(0.1)  # 100ms for socket is enough
+                    if sock.connect_ex((host, port)) == 0:
+                        return
+            except OSError:
+                # not ready yet
+                ...
+        # timeout exceed
+        raise RuntimeError("Couldn't start Pub/Sub Emulator")
+
+    mocker.patch.dict(
+        os.environ,
+        {
+            "PUBSUB_EMULATOR_HOST": f"{host}:{port}",
+            "GOOGLE_CLOUD_PROJECT": "test-project",
+        },
+    )
+    
+    try:
+        await wait_for_emulator()
+        yield proc
+    finally:
+        proc.terminate()
+        try:
+            await asyncio.wait_for(proc.wait(), timeout=timeout_sec)
+        except asyncio.TimeoutError:
+            proc.kill()
+            await proc.wait()
