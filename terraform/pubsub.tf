df184490e3bedb752174960f7823c27382d8564f
Dmitry Mittov <mittov@gmail.com>
2025-12-27 15:21:35 +0100

    add pubsub message topic and subscription

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -10,5 +10,18 @@ resource "google_pubsub_subscription" "sub_explain" {
   ack_deadline_seconds         = 600
   retain_acked_messages        = false
   enable_exactly_once_delivery = true
+}
 
+resource "google_pubsub_topic" "topic_message" {
+  name = "message"
+  message_retention_duration = "604800s" # 7 days
+}
+
+resource "google_pubsub_subscription" "sub_message" {
+  name  = "sub-message-pull"
+  topic = google_pubsub_topic.topic_message.name
+
+  ack_deadline_seconds         = 600
+  retain_acked_messages        = false
+  enable_exactly_once_delivery = true
 }
